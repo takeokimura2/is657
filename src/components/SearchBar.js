b470a1d6cfce9e8e1fc454c0f920678e0d9e6512
Takeo Kimura <takeo.kimura@student.csulb.edu>
2021-06-23 21:59:09 -0700

    Completed Search Bar function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-function SearchBar () {
+function SearchBar ({term, onTermChange, onTermSubmit}) {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle}/>
       <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
         style = {styles.inputStyle} 
         placeholder = "Search"
+        value = {term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+        onEndEditing={() => onTermSubmit()}
       />
     </View>
   )
